fb660193b124a42c681ec497c4df89b20f04a243
icaras84 <icaras84@gmail.com>
2022-12-07 15:51:56 -0800

    InconspicuousOpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/powerplay2022/teleop/production/InconspicuousOpMode.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.robot.powerplay2022.teleop.production;
+
+import org.firstinspires.ftc.teamcode.utils.momm.LoopUtil;
+
+public class InconspicuousOpMode extends LoopUtil {
+
+    @Override
+    public void opInit() {
+
+    }
+
+    @Override
+    public void opInitLoop() {
+
+    }
+
+    @Override
+    public void opStart() {
+
+    }
+
+    @Override
+    public void opUpdate(double deltaTime) {
+
+    }
+
+    @Override
+    public void opFixedUpdate(double deltaTime) {
+
+    }
+
+    @Override
+    public void opStop() {
+
+    }
+}
